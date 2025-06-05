15e7df316fa23b7a8f3d6517ed0eff5f2d90f25f
anastasiiaManilnikova <anastasiia.manilnikova@skycell.ch>
2025-06-05 18:10:58 +0300

    task_3

--- 03-di/01-notification-service/tasks/tasks.service.ts
+++ 03-di/01-notification-service/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto, Task, TaskStatus, UpdateTaskDto } from "./task.model";
+import { NotificationsService } from "../notifications/notifications.service";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private readonly notificationsService: NotificationsService, private readonly usersService: UsersService) {}
 
   async createTask(createTaskDto: CreateTaskDto) {
     const { title, description, assignedTo } = createTaskDto;
@@ -18,6 +20,14 @@ export class TasksService {
     };
     this.tasks.push(task);
 
+    const user = this.usersService.getUserById(task.assignedTo);
+
+    if (!user) {
+      throw new NotFoundException('404 Not Found');
+    }
+
+    this.notificationsService.sendEmail(user.email, 'Новая задача', `Вы назначены ответственным за задачу: "${task.title}"`);
+
     return task;
   }
 
@@ -28,6 +38,15 @@ export class TasksService {
     }
 
     Object.assign(task, updateTaskDto);
+
+    const user = this.usersService.getUserById(task.assignedTo);
+
+    if (!user) {
+      throw new NotFoundException('404 Not Found');
+    }
+
+    this.notificationsService.sendSMS(user.phone, `Статус задачи "${task.title}" обновлён на "completed"`)
+
     return task;
   }
 }
